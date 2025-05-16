033dd026a2a1543a3c07afd26d3be007a514c564
ClyZeR1 <sergeistarodyb@gmail.com>
2025-05-16 12:34:04 +0300

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Books.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Books
+    {
+        protected List<string> BookNames = new List<string>();
+
+        // Делегат
+        public delegate void BookHandler(string text);
+
+        // События
+        public event BookHandler onAddBook;
+        public event BookHandler onDeleteBook;
+        public void AddBook(string bookName)
+        {
+            BookNames.Add(bookName);
+            onAddBook?.Invoke("Книга " + bookName + " добавлена");
+        }
+
+        public void DeleteBook(string bookName)
+        {
+            if (BookNames.Contains(bookName))
+            {
+                BookNames.Remove(bookName);
+                onDeleteBook?.Invoke("Книга " + bookName + " УДАЛЕНА!");
+            }
+            else
+            {
+                Console.WriteLine("Книга " + bookName + " не найдена.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+
+            books.onAddBook += ShowMessage;
+            books.onDeleteBook += ShowMessage;
+
+            books.AddBook("книга1");
+            books.AddBook("книга2");
+
+            books.DeleteBook("книга2");
+            books.DeleteBook("Новая книга"); // Попытка удаления
+        }
+
+        public static void ShowMessage(string text)
+        {
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
